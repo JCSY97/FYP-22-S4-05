5b0c914c58e4a81a667afbeaa1e6f992ba34be5f
Yangtao98 <yangtao1998@live.com>
2023-02-10 09:33:53 +0800

    password

--- fyp_src_1/static/assets/js/main.js
+++ fyp_src_1/static/assets/js/main.js
@@ -273,19 +273,14 @@ $('#status').on('change', function () {
 /**
    * check password
    */
+function matchpassword() {
+  var password = document.getElementById("newPassword")
+    , confirm_password = document.getElementById("renewPassword");
 
-var password = document.getElementById("newPassword")
-  , confirm_password = document.getElementById("renewPassword");
-
-function validatePassword() {
   if (password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Passwords Don't Match");
-  } else {
-    confirm_password.setCustomValidity('');
+    alert("Passwords Don't Match");
   }
+
+
+
 }
-
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
-
-
